61a56be1e0b7bbdd08c05b5e6020ee282749ff69
art-ewantes <webrawwwr@gmail.com>
2018-01-12 22:13:50 +0200

    add client table and client form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 // AngularFire inports 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabase } from 'angularfire2/database-deprecated'; 
@@ -28,7 +29,8 @@ import { ClientService } from './services/client.service';
 const appRoutes: Routes = [ 
   {path: '', component: DashboardComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'add-client', component: AddClientComponent}
 ];
 
 export const firebaseConfig = {
@@ -57,7 +59,8 @@ export const firebaseConfig = {
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
-    AngularFireModule.initializeApp(firebaseConfig)
+    AngularFireModule.initializeApp(firebaseConfig),
+    FormsModule
   ],
   providers: [
     AngularFireAuth,
